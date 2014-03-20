a3247b44a3b3570d26d5acc3c8c853d726fb86d3
Yahir De Leon <ydeleon6@gmail.com>
2014-03-20 18:26:44 -0500

    Added some stuff into the results page, but AJAX not working. I’m looking into it.

--- www/js/movieRetrieval.js
+++ www/js/movieRetrieval.js
@@ -7,34 +7,37 @@ object. var movie, maybe.
  JSON retrieves 1 object, with 3 name:value pairs.
 
 */
-$(document).ready(function() {
+var $j = jQuery.noConflict();
+
+$j(document).ready(function() {
 //declaring necessary global variables
-var movieTitle;
-var movieRating;
-var actors = [];
+
+                   $j.support.cors = true;
 var movieJSON;
+var data; //will hold the json obj with the data.
                   
     //here we get the movie title.
-    $('input').keyup(function() {
-            movieTitle = $(this).val(); //getting movie title, one letter at a time.
+    $j('input').keyup(function() {
+            movieTitle = $j(this).val(); //getting movie title, one letter at a time.
     });
                   
-    $('#go').click(function() {
+    $j('#go').click(function() {
         movieTitle = encodeURI(movieTitle);
         console.log("Click was registered");
-        $.ajax({
+        $j.ajax({
             url:'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=cq8unxj24dtamwv2fwjwqdmq&q='+movieTitle+'&page_limit=4',
             dataType: 'jsonp',
             async: 'false',
             success: function(JSONObject) {
-               //$('#title').append(JSONObject.movies[0].title);
+               $j('#title').append(JSONObject.movies[0].title);
                console.log("1");
-               //$('#rating').append(JSONObject.movies[0].ratings.critics_score);
+               $j('#rating').append(JSONObject.movies[0].ratings.critics_score);
                console.log("2");
-               //$('#actor1').append(JSONObject.movies[0].abridged_cast[0].name);
+               $j('#actor1').append(JSONObject.movies[0].abridged_cast[0].name);
                console.log("3");
-               //$('#actor2').append(JSONObject.movies[0].abridged_cast[1].name);
+               $j('#actor2').append(JSONObject.movies[0].abridged_cast[1].name);
                console.log("4");
+               #j('#actor3').append(JSONobject.movies[0].abridged_cast[2].name);
                console.log("Done.");
                //saving the JSON from Rotten Tomatoes in data variable
                saveObj(JSONObject);
