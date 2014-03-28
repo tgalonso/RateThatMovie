847c67cbfdb484b333e1b6284ba39eb22dd14788
Michelle <tgalonso@yahoo.com>
2014-03-27 21:47:35 -0500

    updated formatting on movie listing

--- www/js/movieRetrieval.js
+++ www/js/movieRetrieval.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 $.support.cors = true; //setting jquery variable to allow cross-domain calls
 var movieJSON;
 var data; //will hold the json obj with the data.
+var output='<ul data-role="listview" data-filter="true">';
                   
     //here we get the movie title.
     $('input').keyup(function() {
@@ -20,11 +21,21 @@ var data; //will hold the json obj with the data.
             success: function(JSONObject) {
                //saving the JSON from Rotten Tomatoes in data variable
                for(var i=0; i<4; i++) {
-                    $('#pic'+i).html("<img src=\""+JSONObject.movies[i].posters.profile+"\"/>");
-                    $('#title'+i).html(JSONObject.movies[i].title);
-                    $('#year'+i).html(JSONObject.movies[i].year);
+                   output += '<li>';
+                    //$('#pic'+i).html("<img src=\""+JSONObject.movies[i].posters.profile+"\"/>");
+                    output += "<img id=\"moviepic\" class=\"moviepic\" src=\""+JSONObject.movies[i].posters.profile+"\"/>";
+                    //$('#title'+i).html(JSONObject.movies[i].title);
+                   output += " ";
+                   output += JSONObject.movies[i].title;
+                    //$('#year'+i).html(JSONObject.movies[i].year);
+                   output += " ";
+                   output += JSONObject.movies[i].year;
+                   output += '</li>';
                }
+              // </ul>
                saveObj(JSONObject);
+               output+='</ul>';
+               $('#postlist').html(output);
             }
         });
         
