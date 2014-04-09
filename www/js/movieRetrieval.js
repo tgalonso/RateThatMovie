8237b96cbb14ea5012693c6bcd475f895650afe1
Yahir De Leon <ydeleon6@gmail.com>
2014-04-08 23:07:45 -0500

    Formatted the results page better and the radio buttons work as expected.

--- www/js/movieRetrieval.js
+++ www/js/movieRetrieval.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
 $.support.cors = true; //setting jquery variable to allow cross-domain calls
 var movieJSON;
 var data; //will hold the json obj with the data.
-//var output='<ul data-role="listview" data-filter="true">';
                   
     //here we get the movie title.
     $('input').keyup(function() {
@@ -21,17 +20,9 @@ var data; //will hold the json obj with the data.
             success: function(JSONObject) {
                //saving the JSON from Rotten Tomatoes in data variable
                for(var i=0; i<4; i++) {
-                   //output += '<li>';
-                    //$('#pic'+i).html("<img src=\""+JSONObject.movies[i].posters.profile+"\"/>");
                     $('#pic'+i).attr("src", JSONObject.movies[i].posters.profile);
-                    //output += "<img id=\"moviepic\" class=\"moviepic\" src=\""+JSONObject.movies[i].posters.profile+"\"/>";
                     $('#title'+i).html(JSONObject.movies[i].title);
-                   //output += " ";
-                   //output += JSONObject.movies[i].title;
                     $('#year'+i).html(JSONObject.movies[i].year);
-                   //output += " ";
-                   //output += JSONObject.movies[i].year;
-                   //output += '</li>';
                }
    
                saveObj(JSONObject);
@@ -42,26 +33,24 @@ var data; //will hold the json obj with the data.
     });
                 
     //represent column and name
-    var col, name;
+    var col, button;
     //this does the mutual excusivity for buttons
     $("input[type=radio]").click(function() {
-            name = $(this);
-            col = name.data("col");
-            
-            $("input[data-col=" + col + "]").prop("checked", false);
-            name.prop("checked", true);
-            //Necessary line. Quirk in JQueryMobile requires this line whenever radiobutton stuff is done with JS                                 
-            $("input[type='radio']").attr("checked",true).checkboxradio("refresh");
+        button = $(this);
+        col = button.data("col");
+        //here we check to see if a button at that column is already checked. If so, make it unchecked.
+        $("input[data-col=\"" + col + "\"]").prop("checked", false);
+        button.prop("checked", true);
+        //jQuery Mobile radiobuttons need this line to reflect changes done after clicks.
+        $("input[type='radio']").checkboxradio("refresh");
                                  
     });
 
-                  
+     //This function just saves the data in a variable so we can use it later.
     function saveObj(JSONObject) {
         data = JSONObject;
     };
-    
-    
-    //Assuming we'll only be using the one html file, we can use data to populate other stuff/fields in rotten tomatoes.
+
 
 
 }); //end of jquery doc
